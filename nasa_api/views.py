2fd9f041caf633ffc2efb76a06f35dd76f85eabd
Rafal <rafaltomaszewsky@gmail.com>
2023-03-23 16:51:17 +0100

    nasa photo of the day view updated

--- nasa_api/views.py
+++ nasa_api/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import requests
 
 def index(request):
-    return render(request, 'nasa_api_templates/index.html')
+    key = 'cFKcJgtVk819w8bOr5iM5UJv07wsrR0vPamoy0fx'
+    url_potd = "https://api.nasa.gov/planetary/apod"
+    params = {
+        'api_key':key
+                }
+    response = requests.get(url_potd, params=params)
+    data = response.json()
+    
+    return render(request, 'nasa_api_templates/index.html',{'data':data})

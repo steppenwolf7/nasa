647dc36991c5cc12655f1e08f7ef54580fcf5681
Rafal <rafaltomaszewsky@gmail.com>
2023-03-24 18:24:44 +0100

    FullHD view done

--- nasa_api/views.py
+++ nasa_api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 import requests
 
 def index(request):
@@ -11,4 +11,33 @@ def index(request):
     response = requests.get(url_potd, params=params)
     data = response.json()
     
-    return render(request, 'nasa_api_templates/index.html',{'data':data})
+    copyright = data["copyright"]
+    explanation = data["explanation"]
+    hdurl = data["hdurl"]
+    title = data["title"]
+
+    my_context = {
+        'copyright': copyright,
+        'explanation':explanation, 
+        'hdurl': hdurl, 
+        'title:':title
+                 }
+    return render(request, 'nasa_api_templates/index.html', context=my_context)
+
+
+def fullhd(request):
+    key = 'cFKcJgtVk819w8bOr5iM5UJv07wsrR0vPamoy0fx'
+    url_potd = "https://api.nasa.gov/planetary/apod"
+    params = {
+        'api_key':key
+                }
+    response = requests.get(url_potd, params=params)
+    data = response.json()
+    
+    hdurl = data["hdurl"]
+   
+    my_context = {
+        'hdurl': hdurl, 
+                }
+    return render(request, 'nasa_api_templates/fullhd.html', context=my_context)
+

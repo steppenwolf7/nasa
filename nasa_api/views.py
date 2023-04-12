58a273edf033f847a5b0b1712903c9ba69741c61
Rafal <rafaltomaszewsky@gmail.com>
2023-04-12 14:43:04 +0200

    code comment

--- nasa_api/views.py
+++ nasa_api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 import requests
 from django.http import JsonResponse
 
-def index(request):
+def index(request):                                             # view which ask api for latitude and longitude and rander index.html
     url = "http://api.open-notify.org/iss-now.json"
     response = requests.get(url)
     data = response.json()
@@ -11,23 +11,23 @@ def index(request):
     longitude = data['iss_position']['longitude']
     
  
-    context = {
+    context = {                                                  #make context dictionary with all my data                   
         'latitude':latitude,
         'longitude':longitude
              }
     return render(request, 'nasa_api_templates/index.html', context=context)
 
-def iss_ask(request):
+def iss_ask(request):                                             #Make a request to the API to get the ISS position data
     url = "http://api.open-notify.org/iss-now.json"
     response = requests.get(url)
     data = response.json()
     
-    latitude = data['iss_position']['latitude']
+    latitude = data['iss_position']['latitude']                    # Extract latitude and longitude from the response
     longitude = data['iss_position']['longitude']
     minus_sign1 = latitude[0]
     minus_sign2 = longitude[0]
     
-    letter1 = None
+    letter1 = None                                                 #Determine if latitude and longitude are in the northern/southern or eastern/western 
     if minus_sign1 == "-":
         letter1 = "S"
     else:
@@ -39,17 +39,17 @@ def iss_ask(request):
     else:
         letter2 = "E"
     
-    context = {
+    context = {                                                     #make context dictionary with all my data
         'latitude':latitude,
         'longitude':longitude, 
         'letter1':letter1,
         'letter2':letter2
               }
-    return JsonResponse(context, content_type='application/json')
+    return JsonResponse(context, content_type='application/json')    # Return the context as a JSON response
 
 
 def nasa_photo(request):
-    key = 'cFKcJgtVk819w8bOr5iM5UJv07wsrR0vPamoy0fx'
+    key = 'cFKcJgtVk819w8bOr5iM5UJv07wsrR0vPamoy0fx'        #Make a request to the API to get data
     url = "https://api.nasa.gov/planetary/apod"
     params = {
         'api_key':key
@@ -57,7 +57,7 @@ def nasa_photo(request):
     response = requests.get(url, params=params)
     data = response.json()
     
-    if "copyright" in data:
+    if "copyright" in data:                                   #checking if "copyright" exist in "data" if not make it None
         copyright = data["copyright"]
     else:
         copyright = "None"
@@ -66,7 +66,7 @@ def nasa_photo(request):
     hdurl = data["hdurl"]
     title = data["title"]
     
-    my_context = {
+    my_context = {                                          #make context dictionary with all my data
         'copyright':copyright,
         'explanation':explanation, 
         'hdurl':hdurl, 
@@ -75,8 +75,8 @@ def nasa_photo(request):
     return render(request, 'nasa_api_templates/nasa_photo.html', context=my_context)
 
 
-def fullhd(request):
-    key = 'cFKcJgtVk819w8bOr5iM5UJv07wsrR0vPamoy0fx'
+def fullhd(request):                                        
+    key = 'cFKcJgtVk819w8bOr5iM5UJv07wsrR0vPamoy0fx'        #Make a request to the API to get data
     url = "https://api.nasa.gov/planetary/apod"
     params = {
         'api_key':key
@@ -84,7 +84,7 @@ def fullhd(request):
     response = requests.get(url, params=params)
     data = response.json()
     
-    hdurl = data["hdurl"]
+    hdurl = data["hdurl"]                                   # getting photo in full hd
    
     my_context = {
         'hdurl': hdurl, 

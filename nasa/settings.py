0226cec31bb790a69624bff9ffb9fdebb4fb3813
Rafał Nordberg <rafaltomaszewsky@gmail.com>
2023-04-12 15:13:02 +0200

    Update settings.py

--- nasa/settings.py
+++ nasa/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&$^2js$!1s1n(0%iul=_u&&&%c6^#k9gb#cwp#=bf6j7hh5_m9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
